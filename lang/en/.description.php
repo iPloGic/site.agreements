7c19dc48e37a05ceaa8cdd09aa08b71075e5e4cf
Konstantin <kvt@iplogic.ru>
2023-04-20 13:28:36 +0300

    Update 20.04.23

--- lang/en/.description.php
+++ lang/en/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS ['COMPONENT_NAME'] = 'Output of site agreements';
-$MESS ['COMPONENT_DESCRIPTION'] = 'Output of a block with information about the agreements that the site visitor accepts while on it';
+$MESS ['IPL_SAG_COMPONENT_NAME'] = 'Output of site agreements';
+$MESS ['IPL_SAG_COMPONENT_DESCRIPTION'] = 'Output of a block with information about the agreements that the site visitor accepts while on it';
 ?>
